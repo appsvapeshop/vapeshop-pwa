62c4ab7f5c7c456fccd15f0aab3fc265773e9d30
p.kania <p.kania@accenture.com>
2024-12-13 13:02:18 +0100

    Fix type error

--- src/exceptions/ValidationError.ts
+++ src/exceptions/ValidationError.ts
@@ -1,7 +1,7 @@
 class ValidationError extends Error {
   constructor(public message: string) {
     super(message)
-    this.stack = (<any>new Error()).stack
+    this.stack = new Error().stack
   }
 }
 
