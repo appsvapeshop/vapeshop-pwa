77ca037c11006be5a4a977bf2266671ab68e650a
kkaniap <xkaniax@gmail.com>
2024-12-24 12:27:18 +0100

    number field added

--- src/components/ui/NumberField/NumberField.tsx
+++ src/components/ui/NumberField/NumberField.tsx
@@ -1,28 +1,48 @@
 import { FC } from 'react'
 import classes from './NumberField.module.css'
+import { FaPlus, FaMinus } from 'react-icons/fa6'
 import { TextField as MuiTextField } from '@mui/material'
+import TappedComponent from '../../animations/TappedComponent/TappedComponent'
 
 type Props = {
   label?: string
   value?: number | undefined
   slotProps?: any
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   styles?: React.CSSProperties
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  incrementHandler?: () => void
+  decrementHandler?: () => void
 }
 
-const NumberField: FC<Props> = ({ label, value, onChange, slotProps, styles }) => {
+const NumberField: FC<Props> = ({
+  label,
+  value,
+  slotProps,
+  styles,
+  onChange,
+  decrementHandler,
+  incrementHandler
+}) => {
   return (
-    <MuiTextField
-      className={classes.input}
-      label={label}
-      variant="outlined"
-      color="secondary"
-      type="number"
-      value={value || ''}
-      onChange={onChange}
-      slotProps={slotProps}
-      style={styles}
-    />
+    <div className={classes.quantity}>
+      <TappedComponent onClick={decrementHandler}>
+        <FaMinus size="1rem" color="var(--inactive-icon-color)" />
+      </TappedComponent>
+      <MuiTextField
+        className={classes.input}
+        label={label}
+        variant="outlined"
+        color="secondary"
+        type="number"
+        value={value || ''}
+        onChange={onChange}
+        slotProps={slotProps}
+        style={styles}
+      />
+      <TappedComponent onClick={incrementHandler}>
+        <FaPlus size="1rem" color="var(--inactive-icon-color)" />
+      </TappedComponent>
+    </div>
   )
 }
 
