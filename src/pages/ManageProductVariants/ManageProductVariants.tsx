2989b87f0ce4e317892fde6b684efc17bbae3d2a
appsvapeshop <apps.vapeshop@gmail.com>
2025-02-10 20:09:56 +0100

    - Updated page Manage Product variants

--- src/pages/ManageProductVariants/ManageProductVariants.tsx
+++ src/pages/ManageProductVariants/ManageProductVariants.tsx
@@ -1,18 +1,47 @@
-import { useParams } from 'react-router-dom'
+import { Skeleton } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 import classes from './ManageProductVariants.module.css'
 import AddCard from '../../components/ui/AddCard/AddCard'
+import { ProductVariant } from '../../types/ProductVariant'
+import { getProductVariants } from '../../utils/productsHelper'
 import TappedComponent from '../../components/animations/TappedComponent/TappedComponent'
 
 const ManageProductVariants = () => {
-  const variants = ['wariant 1', 'wariant 2', 'wariant 3', 'wariant 4']
   const { productId } = useParams()
+  const navigation = useNavigate()
+  const [isLoading, setIsLoading] = useState(true)
+  const [variants, setVariants] = useState<ProductVariant[]>([])
+
+  useEffect(() => {
+    getProductVariants(productId!)
+      .then((variantsSnapshot) => setVariants(variantsSnapshot))
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }, [productId])
 
   return (
     <div className={classes.container}>
-      <AddCard className={classes['add-card']}/>
-      {variants.map(variant => (
-        <TappedComponent className={classes.variant}>{variant}</TappedComponent>
-      ))}
+      {isLoading && (
+        <div className={classes.skeletons}>
+          {[...Array(3)].map((_element, index) => (
+            <Skeleton key={index} />
+          ))}
+        </div>
+      )}
+
+      {!isLoading && (
+        <>
+          <AddCard className={classes['add-card']} onClick={() => navigation('new')} />
+          {variants.map((variant, index) => (
+            <TappedComponent key={index} className={classes.variant} onClick={() => navigation(variant.id)}>
+              {variant.name}
+            </TappedComponent>
+          ))}
+        </>
+      )}
     </div>
   )
 }
