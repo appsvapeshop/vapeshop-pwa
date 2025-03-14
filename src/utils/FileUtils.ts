c35834be6466b25961a1f54b1d5883ba7fbb769b
appsvapeshop <apps.vapeshop@gmail.com>
2025-03-14 16:04:25 +0100

    - File utils

--- src/utils/FileUtils.ts
+++ src/utils/FileUtils.ts
@@ -1,4 +1,9 @@
-export const getImageData = async (url: string) => {
+/**
+ * Download file from given URL and return data that represents that file
+ *
+ * @return data that represents file from given URL
+ */
+export const getFileData = async (url: string) => {
   const response = await fetch(url)
   const blob = await response.blob()
   return await new Promise((callback) => {
@@ -6,6 +11,7 @@ export const getImageData = async (url: string) => {
     reader.onload = function () {
       callback(this.result)
     }
+
     reader.readAsDataURL(blob)
   })
 }
