4f3776504f9bba91b20fd83c3ea481e43a94a9b9
appsvapeshop <apps.vapeshop@gmail.com>
2025-03-14 16:06:19 +0100

    - Platform utils

--- src/utils/PlatformUtils.ts
+++ src/utils/PlatformUtils.ts
@@ -1,6 +1,11 @@
 import { Platform } from '../enums/Platform'
 import { isAndroid, isIOS } from 'mobile-device-detect'
 
+/**
+ * Get current platform
+ *
+ * @return current platform enum
+ */
 export const getPlatform = (): Platform => {
   if (isAndroid) {
     return Platform.Android
